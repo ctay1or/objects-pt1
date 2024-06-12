0f4ef7bc3166b3fe1a44161985ec13c7363d81a6
ctaylor <hi.ctaylor.co@gmail.com>
2024-06-12 17:53:46 -0500

    Added JS code for learning object

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {};
+
+learning.topic = "JS";
+
+learning.learningGoals = [
+    "Finish JS & React Classes",
+    "Build a Portfolio Website",
+    "Learn WordPress",
+    "Get Hired and Do a Happy Dance :)",
+  ];
+
+learning.category = "Front End Development";
+
+learning.topicImportance = "High";
+
+console.log(learning);
+
+learning.topic = "JavaScript";
+
+learning.learningGoals.splice(1, 1);
+
+console.log(learning);
+
+topicElement.innerText = `I'm learning ${learning.topic}.`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `I have ${learning.learningGoals.length} learning goals.`
+countElement.classList.remove("hide");
